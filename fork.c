42ad4d61a05a7fac7815da89b0085c997f0d6be9
erickadikah <erickadikah2030@gmail.com>
2022-08-08 11:17:32 -0700

    initial

--- fork.c
+++ fork.c
@@ -1,22 +1,52 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - fork example
+ *
+ * Return: Always 0.
+ */
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - fork example
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-        pid_t pid;
+    pid_t my_pid;
+    pid_t child_pid;
 
-        printf("before fork iwas one\n");
+    if (child_pid == -1)
+    {
+        perror("Error:");
+        return (1);
+    }
+    my_pid = getpid();
+    printf("My pid is %u\n", my_pid);
+    if (child_pid == 0)
+    {
+int main(void)
+{
+    pid_t my_pid;
+    pid_t child_pid;
 
-        fork();
-
-        if(pid == -1)
-        {
-                perror("unseccessful\n");
-                return 1;
-        }
-
-        printf("after fork i became two\n");
-
-        return 0;
+    if (child_pid == -1)
+    {
+        perror("Error:");
+        return (1);
+    }
+    my_pid = getpid();
+    printf("My pid is %u\n", my_pid);
+    if (child_pid == 0)
+    {
+        printf("(%u) Nooooooooo!\n", my_pid);
+    }
+    else
+    {
+        printf("(%u) %u, I am your father\n", my_pid, child_pid);
+    }
+    return (0);
 }
-
